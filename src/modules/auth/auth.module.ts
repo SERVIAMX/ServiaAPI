97cd74e9056ea148f05b60a3b79928b547108d93
John Doe <johndoe@example.com>
2026-04-14 16:58:40 -0600

    [ADD] 1 dia de token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -20,7 +20,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>('JWT_ACCESS_SECRET', ''),
         signOptions: {
-          expiresIn: config.get<string>('JWT_ACCESS_EXPIRES_IN', '15m'),
+          expiresIn: config.get<string>('JWT_ACCESS_EXPIRES_IN', '1d'),
         },
       }),
     }),
